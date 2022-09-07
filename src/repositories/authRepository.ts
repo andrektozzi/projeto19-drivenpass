dea2a099383ef98fd997e566f5e03addcc6fb892
andrektozzi <andrektozzi@gmail.com>
2022-09-07 10:16:04 -0300

    feat: create tables

---
+++ src/repositories/authRepository.ts
@@ -0,0 +1,19 @@
+import { CreateUserData } from "../services/authService.js";
+import client from "../dbStrategy/database.js";
+
+export async function verifyUserByEmail(email: string) {
+    return await client.user.findUnique({ where: {email}})
+}
+
+export async function createUser({ email, password } : CreateUserData) {
+    await client.user.create({
+        data: {
+            email,
+            password
+        }
+    });
+}
+
+export async function verifyUserByEmailForSignIn(email: string) {
+    return await client.user.findUnique({ where: { email }});
+}
