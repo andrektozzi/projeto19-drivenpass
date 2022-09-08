d3fc8f5bd621a636f3ae483a5b00fd55d5d3eabd
andrektozzi <andrektozzi@gmail.com>
2022-09-08 11:59:39 -0300

    feat: add get wifis

--- src/controllers/wifiController.ts
+++ src/controllers/wifiController.ts
@@ -7,3 +7,9 @@ export async function createWifi(req: Request, res: Response){
     await wifiService.createWifi({userId,title, networkName, password});
     res.sendStatus(201);
 }
+
+export async function fetchWifis(req: Request, res: Response){
+    const userId:number = res.locals.userId;
+    const wifis = await wifiService.fetchWifis(userId);
+    res.status(200).send(wifis);
+}
--- src/routes/wifiRouter.ts
+++ src/routes/wifiRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createWifi } from "../controllers/wifiController.js";
+import { createWifi, fetchWifis } from "../controllers/wifiController.js";
 import { tokenValidator } from "../middlewares/tokenMiddleware.js";
 import { wifiValidator } from "../middlewares/wifiMiddleware.js";
 
 const wifiRouter = Router();
 
 wifiRouter.post("/wifi", tokenValidator, wifiValidator, createWifi);
+wifiRouter.get("/wifi", tokenValidator, fetchWifis);
 
 export default wifiRouter;
