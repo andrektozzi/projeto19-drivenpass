487d539944d07e692af46b4c292d27021df9d26b
andrektozzi <andrektozzi@gmail.com>
2022-09-09 10:45:09 -0300

    fix: safe notes schema

--- src/controllers/safeNotesController.ts
+++ src/controllers/safeNotesController.ts
@@ -4,7 +4,7 @@ import * as noteService from "../services/safeNotesService.js"
 export async function createNote(req: Request, res: Response){
     const { title, note } = req.body;
     const userId:number = res.locals.userId;
-    await noteService.createNote({userId,title, note});
+    await noteService.createNote({userId, title, note});
     res.sendStatus(201);    
 }
 
